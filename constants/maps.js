1e939cea0da75fdb40334100b1e679ade679bbcc
haryobamy <haryobamy.badmus@gmail.com>
2021-06-23 16:16:14 +0100

    Created a new Expo app

---
+++ constants/maps.js
@@ -0,0 +1,3 @@
+const GOOGLE_API_KEY = "AIzaSyBqDQBLtTMWkRtSwpFZtPW1BAbOlvrnivg";
+
+export default GOOGLE_API_KEY;
---
+++ navigation/tabs.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Image, TouchableOpacity } from "react-native";
+
+function tabs() {
+  return (
+    <View>
+      <Text>tabs js</Text>
+    </View>
+  );
+}
+
+export default tabs;
